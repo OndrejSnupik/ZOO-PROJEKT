a28febcae0c296acbd659801a3a0f9beca4a1aa3
OndrejSnupik <151453287+OndrejSnupik@users.noreply.github.com>
2025-12-14 17:34:03 +0100

    Popisky

--- Hallway.cpp
+++ Hallway.cpp
@@ -3,6 +3,7 @@
 //
 #include "Hallway.h"
 
+// Konstruktor předává informace o stěnách rodičovské třídě Tile
 Hallway::Hallway(bool up, bool down, bool left, bool right) : Tile("Chodba") {
     m_exits[0] = up;
     m_exits[1] = down;
